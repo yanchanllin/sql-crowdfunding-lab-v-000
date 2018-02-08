8566bd4a96136bf89204dde22f0827abcbe04555
YanC Lin <yonalin1@gmail.com>
2018-02-08 18:33:28 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,7 +8,7 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "SELECT projects.title,pledges.amount FROM projects JOIN pledges ON projects.id = pledges.project_id
-INNER JOIN users on users.id = pledges.user_id ORDER BY users.name ASC;"
+INNER JOIN users on users.id = pledges.user_id ORDER BY name ASC;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
