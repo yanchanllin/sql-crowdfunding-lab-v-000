b578e9a0e8250da8e584b7e20b641f6ff7f5969c
YanC Lin <yonalin1@gmail.com>
2018-02-08 20:27:15 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -24,7 +24,8 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+"SELECT users.name, SUM(pledges.amount) FROM projects JOIN pledges ON projects.id = pledges.project_id
+INNER JOIN users ON users.id = pledges.user_id GROUP BY users.name ORDER BY pledges.amount,users.name;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
