adbadb0284f190da282fc2da5baa8a0a0222b33b
YanC Lin <yonalin1@gmail.com>
2018-02-08 21:08:14 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -22,10 +22,10 @@ ORDER BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT title, SUM(pledges.amount) > funding_goal FROM projects
+"SELECT title, SUM(pledges.amount) >= funding_goal FROM projects
  JOIN pledges ON pledges.project_id = projects.id
  GROUP BY projects.title
- HAVING SUM(pledges.amount) > projects.funding_goal;"
+ HAVING SUM(pledges.amount) >= projects.funding_goal;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
