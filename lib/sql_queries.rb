252c8010d1c22bc47def3e77444184fec9da2271
YanC Lin <yonalin1@gmail.com>
2018-02-08 20:47:23 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,31 +7,39 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"SELECT projects.title, SUM(pledges.amount) FROM projects JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title
+"SELECT projects.title, SUM(pledges.amount) FROM projects
+JOIN pledges ON projects.id = pledges.project_id
+GROUP BY projects.title
 ORDER BY projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"SELECT users.name, users.age, SUM(pledges.amount) FROM projects JOIN pledges ON projects.id = pledges.project_id
-INNER JOIN users ON users.id = pledges.user_id GROUP BY users.name ORDER BY users.name;"
+"SELECT users.name, users.age, SUM(pledges.amount) FROM projects
+JOIN pledges ON projects.id = pledges.project_id
+INNER JOIN users ON users.id = pledges.user_id
+GROUP BY users.name
+ORDER BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
 "SELECT title, SUM(pledges.amount) >= funding_goal FROM projects
  JOIN pledges ON pledges.project_id = projects.id
- GROUP BY project_id
+ GROUP BY projects.funding_goal
  HAVING SUM(pledges.amount) >= projects.funding_goal;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
 "SELECT name, SUM(pledges.amount) FROM users
 JOIN users ON users.id = pledges.user_id
-GROUP BY user_id
+GROUP BY users.name
 HAVING SUM(pledges.amount),users.name;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+"SELECT category, SUM(pledges.amount) FROM projects
+JOIN pledges ON pledges.project_id = projects.id
+GROUP BY projects.category
+HAVING projects.category = 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
