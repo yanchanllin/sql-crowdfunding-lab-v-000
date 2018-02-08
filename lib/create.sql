5003d36ab7a1c1eefbec3db9a1b24f34471b68ab
YanC Lin <yonalin1@gmail.com>
2018-02-08 21:27:42 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,19 @@
+CREATE TABLE projects (
+id INTEGER PRIMARY KEY,
+title TEXT,
+category INTEGER,
+funding_goal TEXT,
+start_date INTEGER,
+end_date INTEGER
+);
+CREATE TABLE users (
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER
+);
+CREATE TABLE pledges (
+id INTEGER PRIMARY KEY,
+amount INTEGER,
+user_id INTEGER,
+project_id INTEGER
+);
