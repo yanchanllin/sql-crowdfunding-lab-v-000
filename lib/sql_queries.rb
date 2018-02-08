6eff8cffa54eb6b1ac219a985ac6d69b7ac91189
YanC Lin <yonalin1@gmail.com>
2018-02-08 20:10:50 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -17,7 +17,10 @@ INNER JOIN users ON users.id = pledges.user_id GROUP BY users.name ORDER BY user
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, SUM(pledges.amount) FROM projects JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.funding_goal;"
+"SELECT projects.title, SUM(pledges.amount) - funding_goal FROM projects 
+ JOIN pledges ON projects.id = pledges.project_id 
+ GROUP BY projects_id 
+ HAVING SUM(pledges.amount) >= projects.funding_goal;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
