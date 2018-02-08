a0612b6978a729c3b8d0a1a0118e7e0d881d4042
YanC Lin <yonalin1@gmail.com>
2018-02-08 20:52:41 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -38,6 +38,7 @@ end
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
 "SELECT category, pledges.amount FROM projects
 JOIN pledges ON pledges.project_id = projects.id
+GROUP BY project_id
 HAVING category = 'music';"
 end
 
